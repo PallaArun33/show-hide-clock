2b995c0602ff47dbfcae1706166486d3e6a39df6
arundhathi <parun00007@gmail.com>
2023-08-22 12:21:54 +0000

    clock app

---
+++ src/components/Clock/index.js
@@ -0,0 +1,38 @@
+import {Component} from 'react'
+import './index.css'
+
+class Clock extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {date: new Date()}
+    console.log('state or constructor is clicked')
+  } // **  also write as "state = {date: new Date()}"" **//
+
+  componentDidMount() {
+    console.log('componentDidMount is clicked')
+    this.timerId = setInterval(this.tick, 1000)
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount is clicked')
+    clearInterval(this.timerId)
+  }
+
+  tick = () => {
+    console.log('tick presentDate is created')
+    this.setState({date: new Date()})
+  }
+
+  render() {
+    console.log('render is clicked')
+    const {date} = this.state
+
+    return (
+      <div className="clock-container">
+        <h1 className="heading">Clock</h1>
+        <p className="time">{date.toLocaleTimeString()} </p>
+      </div>
+    )
+  }
+}
+export default Clock
